dc7f386559eca7546afb27d0edf15b09420aa896
Francesco Tenchini <francesco.tenchini@desy.de>
2025-03-04 18:59:20 +0100

    removed hardcoded LV,HV, and FC7 values, which we now pull from DB

--- BurnIn_GUI.py
+++ BurnIn_GUI.py
@@ -54,41 +54,14 @@ class BurnIn_GUI(QtWidgets.QMainWindow):
 		self.TestSession="session1" #hardcoded (test value)
 		
 		self.LVNames=["?"] * 10
-		self.LVNames[0]="BLV01"
-		self.LVNames[1]="BLV02"
-		self.LVNames[2]="BLV03"
-		self.LVNames[3]="BLV04"
-		self.LVNames[4]="BLV05"
-		self.LVNames[5]="BLV06"
-		self.LVNames[6]="BLV07"
-		self.LVNames[7]="BLV08"
-		self.LVNames[8]="BLV09"
-		self.LVNames[9]="BLV10"
-	
 		self.HVNames=["?"] * 10
-		self.HVNames[0]="HV001"
-		self.HVNames[1]="HV002"
-		self.HVNames[2]="HV003"
-		self.HVNames[3]="HV004"
-		self.HVNames[4]="HV005"
-		self.HVNames[5]="HV006"
-		self.HVNames[6]="HV007"
-		self.HVNames[7]="HV008"
-		self.HVNames[8]="HV009"
-		self.HVNames[9]="HV010"
-	
 		self.fc7IDs=["?"] * 10
-		self.fc7IDs[0]="fc7ot2"
-		self.fc7IDs[1]="fc7ot2"
-	
 		self.fc7Slots=["?"] * 10
-		self.fc7Slots[0]="0"
-		self.fc7Slots[1]="1"
 		
 		#cooncting to DB for connection
 		self.DB_interface = DB_interface(self.configDict,self.logger)
 		try:
-			self.DB_interface.getConnectionsFromDB(self.fc7IDs,self.fc7Slots)
+			self.DB_interface.getConnectionsFromDB(self.LVNames,self.HVNames,self.fc7IDs,self.fc7Slots)
 		except Exception as e:
 			self.logger.warning(e)
 		
@@ -585,9 +558,11 @@ class BurnIn_GUI(QtWidgets.QMainWindow):
 	
 	def BI_ReloadConn_Cmd(self):
 	
-		self.DB_interface.getConnectionsFromDB(self.fc7IDs,self.fc7Slots)
+		self.DB_interface.getConnectionsFromDB(self.LVNames,self.HVNames,self.fc7IDs,self.fc7Slots)
 		for row in range(10):
-
+                        
+			self.Ctrl_CAEN_table.setItem(row,0,QtWidgets.QTableWidgetItem(self.LVNames[row]))
+			self.Ctrl_CAEN_table.setItem(row,5,QtWidgets.QTableWidgetItem(self.HVNames[row]))
 			self.Ctrl_CAEN_table.setItem(row,10,QtWidgets.QTableWidgetItem(self.fc7IDs[row]))
 			self.Ctrl_CAEN_table.setItem(row,11,QtWidgets.QTableWidgetItem(self.fc7Slots[row]))
 			
